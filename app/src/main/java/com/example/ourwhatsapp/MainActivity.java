40c6473db877782a4570f5e6ea5dff368deaf629
YuvalDahari <yuvaldahari71568@gmail.com>
2023-06-13 02:43:09 +0300

    add vertical

--- app/src/main/java/com/example/ourwhatsapp/MainActivity.java
+++ app/src/main/java/com/example/ourwhatsapp/MainActivity.java
@@ -1,11 +1,12 @@
 package com.example.ourwhatsapp;
 
-import androidx.appcompat.app.AppCompatActivity;
-
 import android.content.Intent;
+import android.content.res.Configuration;
 import android.os.Bundle;
-import android.view.View;
 import android.widget.Button;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -15,29 +16,47 @@ public class MainActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+
+        // Check the screen orientation
+        int orientation = getResources().getConfiguration().orientation;
+
+        // Set the content view based on the orientation
+        if (orientation == Configuration.ORIENTATION_LANDSCAPE) {
+            setContentView(R.layout.activity_main_horizontal);
+        } else {
+            setContentView(R.layout.activity_main);
+        }
 
         showListButton = findViewById(R.id.show_list_button);
 
-        showListButton.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Intent intent = new Intent(getApplicationContext(), ListActivity.class);
-
-                startActivity(intent);
-            }
+        showListButton.setOnClickListener(view -> {
+            Intent intent = new Intent(getApplicationContext(), ListActivity.class);
+            startActivity(intent);
         });
 
         registerPage = findViewById(R.id.navToRegister);
 
-        registerPage.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Intent intent = new Intent(getApplicationContext(), RegisterActivity.class);
-
-                startActivity(intent);
-            }
+        registerPage.setOnClickListener(view -> {
+            Intent intent = new Intent(getApplicationContext(), RegisterActivity.class);
+            startActivity(intent);
         });
     }
 
+    @Override
+    public void onConfigurationChanged(@NonNull Configuration newConfig) {
+        super.onConfigurationChanged(newConfig);
+
+        // Check the new screen orientation
+        if (newConfig.orientation == Configuration.ORIENTATION_LANDSCAPE) {
+            if (isFinishing()) {
+                return;
+            }
+            setContentView(R.layout.activity_main_horizontal);
+        } else if (newConfig.orientation == Configuration.ORIENTATION_PORTRAIT) {
+            if (isFinishing()) {
+                return;
+            }
+            setContentView(R.layout.activity_main);
+        }
+    }
 }
--- app/src/main/java/com/example/ourwhatsapp/RegisterActivity.java
+++ app/src/main/java/com/example/ourwhatsapp/RegisterActivity.java
@@ -1,11 +1,12 @@
 package com.example.ourwhatsapp;
 
-import androidx.appcompat.app.AppCompatActivity;
-
 import android.content.Intent;
+import android.content.res.Configuration;
 import android.os.Bundle;
-import android.view.View;
 import android.widget.Button;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
 
 public class RegisterActivity extends AppCompatActivity {
 
@@ -14,17 +15,39 @@ public class RegisterActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_register);
+        // Check the screen orientation
+        int orientation = getResources().getConfiguration().orientation;
 
+        // Set the content view based on the orientation
+        if (orientation == Configuration.ORIENTATION_LANDSCAPE) {
+            setContentView(R.layout.activity_register_horizontal);
+        } else {
+            setContentView(R.layout.activity_register);
+        }
         loginButton = findViewById(R.id.navToLogin);
 
-        loginButton.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Intent intent = new Intent(getApplicationContext(), MainActivity.class);
+        loginButton.setOnClickListener(view -> {
+            Intent intent = new Intent(getApplicationContext(), MainActivity.class);
 
-                startActivity(intent);
-            }
+            startActivity(intent);
         });
+    }
+
+    @Override
+    public void onConfigurationChanged(@NonNull Configuration newConfig) {
+        super.onConfigurationChanged(newConfig);
+
+        // Check the new screen orientation
+        if (newConfig.orientation == Configuration.ORIENTATION_LANDSCAPE) {
+            if (isFinishing()) {
+                return;
+            }
+            setContentView(R.layout.activity_register_horizontal);
+        } else if (newConfig.orientation == Configuration.ORIENTATION_PORTRAIT) {
+            if (isFinishing()) {
+                return;
+            }
+            setContentView(R.layout.activity_main);
+        }
     }
 }
